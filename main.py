46ed9cc3abf8e7b5fb3467b95172b0925e153b46
Gabriel Carvalho <ghenriquecarvalho96@gmail.com>
2024-03-26 21:38:27 -0300

    Meu primeiro commit

---
+++ main.py
@@ -0,0 +1,18 @@
+print("Jornada de Dados")
+
+# Variáveis
+idade = 27
+nome = "Gabriel"
+
+#Print
+print(idade)
+
+#soma
+nova_idade = idade + 1
+print(nova_idade)
+
+#lista
+lista = [1,2,3,4,5]
+
+#Métodos
+lista_invertida = lista.reverse()
